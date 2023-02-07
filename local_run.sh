cffcafdf8270e64491d026ebec2e23fec89d79f4
Tong LI <tl10@sanger.ac.uk>
2023-02-07 15:24:59 +0000

    enforce NXF_VER to 22.04.5

--- local_run.sh
+++ local_run.sh
@@ -12,7 +12,7 @@ DATE_WITH_TIME=`date "+%Y%m%d%H%M"`
 TRACE_FILE="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_trace/registration_trace_${DATE_WITH_TIME}.tsv"
 TMP_NF_WORK="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_work"
 
-NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK nextflow -trace nextflow.executor run /lustre/scratch117/cellgen/team283/tl10/workflow-assign-peaks-to-cells/main.nf \
+NXF_OPTS='-Dleveldb.mmap=false' NXF_VER=22.04.5 NXF_WORK=$TMP_NF_WORK nextflow -trace nextflow.executor run /lustre/scratch126/cellgen/team283/tl10/workflow-assign-peaks-to-cells/main.nf \
 	-params-file $1 \
 	-profile local \
 	-resume
