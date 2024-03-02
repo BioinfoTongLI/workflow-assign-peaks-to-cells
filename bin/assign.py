bc83dc63a32b4c6e7bb4a2409938938c05be4075
Tong LI <tl10@sanger.ac.uk>
2024-03-02 09:43:49 +0000

    new version (shapely 2.0.3) of STRtree cell/spot assignment. Should be dramatically faster

--- bin/assign.py
+++ bin/assign.py
@@ -13,61 +13,46 @@ import argparse
 import pandas as pd
 import pickle
 import fire
+import numpy as np
 
 
 def main(peaks, cells, stem="out"):
     with open(cells, "rb") as handle:
         cells = pickle.load(handle)
-
+        cell_ids = np.array(list(cells.keys()))
+        cells = np.array(list(cells.values()))
     with open(peaks, "rb") as handle:
         trees = pickle.load(handle)
         if len(trees) == 1:
             trees = trees[0]
 
     spot_counts = {}
-    cell_centroids = {}
-    ys, xs, chs, cell_indexes = [], [], [], []
-    for cell_index in cells:
-        cell = cells[cell_index]
-        if cell.centroid.is_empty: # ignore the shapes that don't have centoird
-           continue
-        cell_centroids[cell_index] = {"y": cell.centroid.y, "x": cell.centroid.x}
+    cell_ys, cell_xs, ys, xs, genes, cell_indexes = [], [], [], [], [], []
 
-        current_counts = {}
-        for ch in trees:
-            potential_inside = trees[ch].query(cell)
-            true_in = [sp for sp in potential_inside if cell.is_valid and cell.contains(sp)]
-            for sp in true_in:
-                ys.append(sp.y)
-                xs.append(sp.x)
-                chs.append(ch)
-                cell_indexes.append(cell_index)
-            current_counts[ch] = len(true_in)
-        spot_counts[cell_index] = current_counts
-        del cell
-        del current_counts
+    for gene in trees:
+        indices_inside = trees[gene].query(cells, predicate="contains")
+        target_cells = cells[indices_inside[0]]
+        target_cell_ids = cell_ids[indices_inside[0]]
+        cell_indexes += target_cell_ids.tolist()
+        target_spots = trees[gene].geometries.take(indices_inside[1])
+        for cell, spot in zip(target_cells, target_spots):
+            ys.append(spot.y)
+            xs.append(spot.x)
+            genes.append(gene)
+            cell_ys.append(cell.centroid.y)
+            cell_xs.append(cell.centroid.x)
 
     spots_df = pd.DataFrame(
-        {"y": ys, "x": xs, "ch": chs, "ID": cell_indexes}
+        {"y": ys, "x": xs, "genes": genes, "ID": cell_indexes, "cell_y": cell_ys, "cell_x": cell_xs}
     ).set_index("ID")
     spots_df.to_csv(f"{stem}_assigned_peaks.csv")
 
-    count_df = pd.DataFrame(spot_counts).T
+    count_df = spots_df.pivot_table(index='ID', columns='genes', aggfunc='size', fill_value=0)
     count_df.to_csv(f"{stem}_peak_counts.csv")
 
-    centroid_df = pd.DataFrame(cell_centroids).T
-    centroid_df.to_csv(f"{stem}_cell_centroids.csv")
-
-    n_total = [trees[i]._n_geoms for i in trees]
-    summary = pd.DataFrame(
-        {
-            "Sum": count_df.sum(),
-            "Total_per_ch": n_total,
-            "Percentage": count_df.sum() / n_total,
-        }
-    )
-    summary.to_csv(f"{stem}_summary.csv")
-
+    cell_coordinates_df = spots_df.groupby("ID")[["cell_y", "cell_x"]].first()
+    cell_coordinates_df.to_csv(f"{stem}_cell_centroids.csv")
+    
 
 if __name__ == "__main__":
     fire.Fire(main)
